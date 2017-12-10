3b6dc9f01f743b6e4b4172e0429b2ab2bc9f3482
Lazarus Lazaridis <lazarus.lazaridis@gmail.com>
2017-12-10 15:35:33 +0200

    Bootstrapped.

---
+++ assets/javascripts/iridakos.js
@@ -0,0 +1,7 @@
+$(function() {
+      $('img').on('click', function() {
+  			$('#image-modal .image').attr('src', $(this).attr('src'));
+        $('#image-modal .modal-title').html($(this).attr('alt'));
+  			$('#image-modal').modal('show');
+  		});
+});
